f644ad45ed9078806ca38af7a4a31992609ba63d
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-02-26 16:18:38 +0200

    product endpoint relates #15

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,11 +2,15 @@ const router = require('express').Router();
 const app = require('../app');
 
 const brandController = require('../controllers/brand');
+const productController = require('../controllers/product');
 
 router.get('/products',(req, res)=>{
     res.send('this is a products')
 })
 
 router.get('/brands', brandController.list);
+
+router.get('/product/list', productController.list);
+router.post('/product/add', productController.add);
 
 module.exports = router;
