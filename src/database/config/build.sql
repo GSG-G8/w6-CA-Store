2aca42a8d9d31e92e447484c3ed9f8f925769996
Ali <ali.dahdouh@gmail.com>
2020-02-29 09:27:17 +0200

    added test file and fix sql relates #41

--- src/database/config/build.sql
+++ src/database/config/build.sql
@@ -3,33 +3,33 @@ BEGIN;
 DROP TABLE IF EXISTS brand, product, cart, tag, product_tag;
 
 CREATE TABLE brand (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE product (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price FLOAT NOT NULL,
     image TEXT NOT NULL,
     description TEXT NOT NULL, 
-    brand_id INTEGER REFERENCES brand(id)
+    brand_id INTEGER
 );
 
 CREATE TABLE cart (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     quantity INTEGER NOT NULL,
-    product_id INTEGER REFERENCES brand(id)
+    product_id INTEGER
 );
 
 CREATE TABLE tag (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE product_tag (
-    product_id INTEGER REFERENCES brand(id),
-    tag_id INTEGER REFERENCES brand(id)
+    product_id INTEGER,
+    tag_id INTEGER
 );
 
 
