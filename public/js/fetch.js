89448d6369463ea913799f1e6eac684477c5aae5
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-02-29 09:19:14 +0200

    fetch file relates #38

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -4,9 +4,41 @@ function fetchProducts() {
         .then(renderProducts)
 }
 
-function addProducts(obj) {
+function fetchTags() {
+    fetch('/tags/list')
+    .then(res => res.json())
+    .then(renderTags);
+}
+
+function fetchTagProducts(tag_id) {
+    fetch("/product_tags/products/"+tag_id)
+        .then(res => res.json())
+        .then(renderProducts)
+}
+
+function fetchCart() {
+    fetch("/cart/list")
+        .then(res => res.json())
+        .then(renderCart)
+}
+
+function addProduct(obj) {
     fetch('/product/add', {
         method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+      })
+      .then(() => {
+        fetchProducts();
+        fetchTags();
+      });
+}
+
+function updateProduct(id, obj) {
+    fetch('/product/update/'+id, {
+        method: 'put',
         headers: {
             'Content-Type': 'application/json'
         },
@@ -15,3 +47,30 @@ function addProducts(obj) {
       .then(fetchProducts);
 }
 
+function deleteProduct(id) {
+    fetch('/product/delete/'+id, {
+        method: 'delete'
+      })
+      .then(() => {
+        fetchProducts();
+        fetchTags();
+      });
+}
+
+function addToCart(obj) {
+    fetch('/cart/add', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+      })
+      .then(fetchCart);
+}
+
+function clearCart(obj) {
+    fetch('/cart/clear', {
+        method: 'delete'
+    })
+    .then(fetchCart);
+}
